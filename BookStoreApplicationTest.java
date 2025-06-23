a8fc885ffc1c462fedd08edfadbe5b09c1b4f32a
omprakashkumar14 <155672096+omprakashkumar14@users.noreply.github.com>
2025-06-23 06:51:15 +0530

    Add files via upload
    2nd commit

---
+++ BookStoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.bookstore;
+
+import org.junit.jupiter.api.Test;
+
+public class BookStoreApplicationTest {
+
+    @Test
+    void mainMethodRuns() {
+        BookStoreApplication.main(new String[] {});
+    }
+}
